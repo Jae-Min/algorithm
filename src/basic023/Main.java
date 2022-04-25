c4ce29065ef78e8b310c358b814695c2b5256868
jaemin <anjm0408@gmail.com>
2022-04-25 17:55:11 +0900

    basic023

---
+++ src/basic023/Main.java
@@ -0,0 +1,32 @@
+package basic023;
+
+import java.util.Scanner;
+
+/**
+ * 입력된 문자열을 뒤집이서 출력하시오
+ * case) abcde
+ * result) edcba
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.print("문자열을 입력하세요: ");
+            Scanner scanner = new Scanner(System.in);
+            logic(scanner.nextLine());
+        }
+    }
+
+    private static void logic(String input) {
+
+        char[] tmp = new char[input.length()];
+        char[] cArr = input.toCharArray();
+        int index = tmp.length - 1;
+        for(int i = 0; i < cArr.length; i++) {
+            tmp[index] = cArr[i];
+            index--;
+        }
+
+        System.out.println(String.copyValueOf(tmp));
+    }
+}
